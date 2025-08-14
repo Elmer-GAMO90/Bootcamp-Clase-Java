dd8d890b5c210b05d102581dee7753a2c005e395
Elmer <lesnar018@gmail.com>
2025-08-13 23:39:59 -0500

    Subiendo array encontrando maxNumber

--- src/ArrayExample2.java
+++ src/ArrayExample2.java
@@ -5,5 +5,18 @@ public class ArrayExample2 {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Ingrese el tamaño del arreglo");
+
+
+        //Nuevo ejemplo, el numero mayor de un array
+        int[] numeros = {20, 54, 99, 21, 41};
+        int max = numeros[0];
+
+        //Recorremos el array para encontrar el mayor
+        for (int numero : numeros) {
+            if (numero > max) {
+                max = numero;
+            }
+        }
+        System.out.println("El número mayor es: " + max);
     }
 }
